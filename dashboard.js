d9a5b4ba722d2a761d9fddfb026fdbd997e44c6d
Artur Belmiro <arturbmelo876@gmail.com>
2026-07-27 20:32:36 -0300

    Finaliza interface do dashboard

--- dashboard.js
+++ dashboard.js
@@ -1,12 +1,11 @@
-// Pega o nome salvo no cadastro
-const nome = localStorage.getItem("nome");
 
-// Procura o lugar onde o nome deve aparecer
-const usuario = document.getElementById("username");
+  const nome = localStorage.getItem("nome");
 
-// Se existir um nome salvo, mostra ele
-if (nome) {
-    usuario.textContent = nome;
+ const usuario = document.getElementById("username");
+
+// Se existir um nome salvo ele mostra ele
+ if (nome) {
+usuario.textContent = nome;
 }
 
 
@@ -15,3 +14,36 @@ const periodo = document.getElementById("periodo");
 periodo.addEventListener("change", function () {
     console.log(periodo.value);
 });
+
+
+const abrir = document.getElementById("abrirPopup");
+const overlay = document.getElementById("overlay");
+const cancelar = document.getElementById("cancelar");
+
+abrir.onclick = () => {
+    overlay.classList.add("ativo");
+}
+
+cancelar.onclick = () => {
+    overlay.classList.remove("ativo");
+}
+
+
+const abrirpopup = document.getElementById("abrirPopup");
+const addmateria = document.getElementById("addmateria");
+const selectMateria = document.getElementById("materiaselect");
+const adicionar = document.getElementById("adicionar");
+const inputMateria = document.getElementById("novaMateria");
+
+adicionar.onclick = () => {
+
+    const overlay = document.getElementById("overlay");
+    const nomeMateria = inputMateria.value;
+
+  const novaOption = document.createElement("option");
+
+ novaOption.textContent = nomeMateria;
+                                                                       
+    selectMateria.appendChild(novaOption);
+ overlay.classList.remove("ativo");  
+}
